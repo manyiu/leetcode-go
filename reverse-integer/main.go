da34dc5e2b813cf6100cee86d02f57a6eaa134cd
Man Yiu <wongmanyiu@gmail.com>
2019-05-26 21:17:01 +0800

    Reverse Integer

--- reverse-integer/main.go
+++ reverse-integer/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func reverse(x int) int {
-	return 0
+	if x > int(math.Pow(2, 31)-1) || x < int(-math.Pow(2, 31)) {
+		return 0
+	}
+
+	y := x
+	n := 0
+
+	if x < 0 {
+		y = -y
+	}
+
+	for y > 0 {
+		r := y % 10
+		n *= 10
+		n += r
+		y /= 10
+	}
+
+	if x < 0 {
+		n = -n
+	}
+
+	if n > int(math.Pow(2, 31)-1) || n < int(-math.Pow(2, 31)) {
+		return 0
+	}
+
+	return n
 }
 
 func main() {
-	fmt.Println(reverse(0))
+	fmt.Println(reverse(-1230343000))
 }
