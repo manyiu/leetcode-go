67cc2bdb9873c8e13b1abfb5a3804e5ed75ee513
Man Yiu <11912398+manyiu@users.noreply.github.com>
2023-08-30 23:32:59 +0800

    876. Middle of the Linked List

---
+++ 876_middle-of-the-linked-list/main.go
@@ -0,0 +1,17 @@
+package middleofthelinkedlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
