eb7b369f9c5dda12981ec008d0940b5a08655479
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-08-24 23:19:19 +0800

    112. Path Sum

---
+++ 112_path-sum/main_test.go
@@ -0,0 +1,37 @@
+package pathsum
+
+import "testing"
+
+func TestHasPathSum(t *testing.T) {
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 11,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 13,
+			},
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+	}
+
+	if hasPathSum(root, 22) != true {
+		t.Fail()
+	}
+}
