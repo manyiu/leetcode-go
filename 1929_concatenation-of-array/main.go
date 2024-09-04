920c884023d7a71fa67f374731cf010fe086802f
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-04 21:47:45 +0800

    1929. Concatenation of Array

--- 1929_concatenation-of-array/main.go
+++ 1929_concatenation-of-array/main.go
@@ -1,7 +1,13 @@
 package main
 
 func getConcatenation(nums []int) []int {
-	return append(nums, nums...)
+	r := make([]int, len(nums)*2)
+
+	for i, v := range nums {
+		r[i], r[len(nums)+i] = v, v
+	}
+
+	return r
 }
 
 func main() {}
