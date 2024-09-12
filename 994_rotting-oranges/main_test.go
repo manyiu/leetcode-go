512765d812267184291668621440166c721528a5
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-12 16:58:30 +0800

    994. Rotting Oranges Add more test cases

--- 994_rotting-oranges/main_test.go
+++ 994_rotting-oranges/main_test.go
@@ -18,4 +18,30 @@ func TestOrangesRotting(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Expected %d, got %d", expected, actual)
 	}
+
+	grid = [][]int{
+		{2, 1, 1},
+		{0, 1, 1},
+		{1, 0, 1},
+	}
+
+	expected = -1
+
+	actual = orangesRotting(grid)
+
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
+
+	grid = [][]int{
+		{0, 2},
+	}
+
+	expected = 0
+
+	actual = orangesRotting(grid)
+
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
