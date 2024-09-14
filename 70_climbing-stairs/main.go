b3497480f4e6548ecf711d6b6c5302a8d4d57f0a
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-14 15:16:45 +0800

    Redo 70. Climbing Stairs

--- 70_climbing-stairs/main.go
+++ 70_climbing-stairs/main.go
@@ -1,13 +1,13 @@
 package climbingstairs
 
 func climbStairs(n int) int {
-	if n <= 1 {
-		return 1
+	if n <= 2 {
+		return n
 	}
 
-	a, b := 1, 1
+	a, b := 1, 2
 
-	for i := 2; i <= n; i++ {
+	for i := 3; i <= n; i++ {
 		a, b = b, a+b
 	}
 
