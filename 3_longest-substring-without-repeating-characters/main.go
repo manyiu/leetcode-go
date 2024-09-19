3b4c6721d8967d5d5abae422456b207e27b314ed
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-19 16:08:54 +0800

    Redo 3. Longest Substring Without Repeating Characters

--- 3_longest-substring-without-repeating-characters/main.go
+++ 3_longest-substring-without-repeating-characters/main.go
@@ -1,21 +1,23 @@
-package main
+package longestsubstringwithoutrepeatingcharacters
 
 func lengthOfLongestSubstring(s string) int {
-	m := 0
-	b := 0
-	h := map[rune]int{}
+	l, r := 0, 0
+	longest := 0
+	m := map[byte]int{}
 
-	for i, v := range s {
-		if p, ok := h[v]; ok && p >= b {
-			b = p + 1
-		} else if i-b+1 > m {
-			m = i - b + 1
+	for r < len(s) {
+		if i, existed := m[s[r]]; existed && i >= l {
+			l = i + 1
 		}
 
-		h[v] = i
+		m[s[r]] = r
+
+		if r-l+1 > longest {
+			longest = r - l + 1
+		}
+
+		r++
 	}
 
-	return m
+	return longest
 }
-
-func main() {}
