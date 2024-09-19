4976e78f915e0241915aa3dd071963b9996a040a
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-20 00:08:07 +0800

    Redo 80. Remove Duplicates from Sorted Array II

--- 80_remove-duplicates-from-sorted-array-ii/main.go
+++ 80_remove-duplicates-from-sorted-array-ii/main.go
@@ -1,20 +1,23 @@
 package removeduplicatesfromsortedarrayii
 
 func removeDuplicates(nums []int) int {
-	l, r := 0, 1
+	l, r := 0, 0
 
 	for r < len(nums) {
-		if nums[l] == nums[r] {
-			if r-l >= 2 {
-				nums = append(nums[:r], nums[r+1:]...)
-			} else {
-				r++
-			}
-		} else {
-			l = r
+		count := 1
+
+		for r+1 < len(nums) && nums[r] == nums[r+1] {
+			count++
 			r++
 		}
+
+		for i := 0; i < 2 && i < count; i++ {
+			nums[l] = nums[r]
+			l++
+		}
+
+		r++
 	}
 
-	return len(nums)
+	return l
 }
