9a49bc1b5bc7297afcdbba30763dcb21efb95321
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-09-20 02:03:24 +0800

    42. Trapping Rain Water Two Pointers

--- 42_trapping-rain-water/main.go
+++ 42_trapping-rain-water/main.go
@@ -1,43 +1,19 @@
 package trappingrainwater
 
 func trap(height []int) int {
+	l, r := 0, len(height)-1
+	maxLeft, maxRight := height[l], height[r]
 	total := 0
 
-	for {
-		l, r := 0, len(height)-1
-
-		for l < r {
-			if height[l] != 0 && height[r] != 0 {
-				break
-			}
-
-			if height[l] == 0 {
-				l++
-			}
-
-			if height[r] == 0 {
-				r--
-			}
-		}
-
-		if r-l <= 1 {
-			break
-		}
-
-		count := 0
-
-		for i := l; i <= r; i++ {
-			if height[i] == 0 {
-				count++
-			}
-		}
-
-		total += count
-
-		for j := 0; j < len(height); j++ {
-			if height[j] > 0 {
-				height[j] -= 1
-			}
+	for l < r {
+		if maxLeft < maxRight {
+			l++
+			maxLeft = max(maxLeft, height[l])
+			total += maxLeft - height[l]
+		} else {
+			r--
+			maxRight = max(maxRight, height[r])
+			total += maxRight - height[r]
 		}
 	}
 
--- 42_trapping-rain-water/main_test.go
+++ 42_trapping-rain-water/main_test.go
@@ -15,6 +15,7 @@ func TestTrap(t *testing.T) {
 		{height: []int{4, 2, 3, 1, 2, 1}, want: 2},
 		{height: []int{4, 2, 3, 1, 2, 1, 3}, want: 6},
 		{height: []int{4, 2, 3, 1, 2, 1, 3, 1}, want: 6},
+		{height: []int{5, 5, 1, 7, 1, 1, 5, 2, 7, 6}, want: 23},
 	}
 
 	for _, tt := range tests {
