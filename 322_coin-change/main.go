45f4fffd8195bd0472affa8469fd5c15b27b96e4
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-10-13 15:18:24 +0800

    322. Coin Change Redo

--- 322_coin-change/main.go
+++ 322_coin-change/main.go
@@ -5,21 +5,16 @@ import "math"
 func coinChange(coins []int, amount int) int {
 	dp := make([]int, amount+1)
 
-	for i := range dp {
+	for i := 0; i < len(dp); i++ {
 		dp[i] = math.MaxInt32
 	}
 
 	dp[0] = 0
 
-	for j := 1; j < len(dp); j++ {
-		for _, coin := range coins {
-			if j-coin >= 0 {
-				cur := dp[j]
-				can := dp[j-coin] + 1
-
-				if can < cur {
-					dp[j] = can
-				}
+	for i := 1; i < len(dp); i++ {
+		for j := 0; j < len(coins); j++ {
+			if i-coins[j] >= 0 {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
 			}
 		}
 	}
--- 322_coin-change/main_test.go
+++ 322_coin-change/main_test.go
@@ -5,11 +5,21 @@ import (
 )
 
 func TestCoinChange(t *testing.T) {
-	input := []int{1, 2, 5}
-	expected := 3
-	actual := coinChange(input, 11)
+	testCases := []struct {
+		coins  []int
+		amount int
+		want   int
+	}{
+		{coins: []int{1, 2, 5}, amount: 11, want: 3},
+		{coins: []int{2}, amount: 3, want: -1},
+		{coins: []int{1}, amount: 0, want: 0},
+		{coins: []int{1}, amount: 1, want: 1},
+		{coins: []int{1}, amount: 2, want: 2},
+	}
 
-	if actual != expected {
-		t.Errorf("Expected %v but was %v", expected, actual)
+	for _, tc := range testCases {
+		if got := coinChange(tc.coins, tc.amount); got != tc.want {
+			t.Errorf("coins: %v, amount: %v, got: %v, want: %v", tc.coins, tc.amount, got, tc.want)
+		}
 	}
 }
