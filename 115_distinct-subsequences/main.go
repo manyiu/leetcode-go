923af7745968383c0a1267878f77bc6d671d7761
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-10-14 23:29:24 +0800

    115. Distinct Subsequences Redo with buttom up

--- 115_distinct-subsequences/main.go
+++ 115_distinct-subsequences/main.go
@@ -1,38 +1,24 @@
 package distinctsubsequences
 
-func dp(s, t string, i, j int, cache [][]int) int {
-	if len(t) == j {
-		return 1
-	}
-	if len(s) == i {
-		return 0
-	}
-
-	if cache[i][j] != -1 {
-		return cache[i][j]
-	}
-
-	if s[i] == t[j] {
-		cache[i][j] = dp(s, t, i+1, j+1, cache) + dp(s, t, i+1, j, cache)
-	} else {
-		cache[i][j] = dp(s, t, i+1, j, cache)
-	}
-
-	return cache[i][j]
-}
-
 func numDistinct(s string, t string) int {
-	cache := make([][]int, len(s)+1)
+	dp := make([][]int, len(s)+1)
 
-	for i := 0; i < len(cache); i++ {
+	for i := 0; i < len(dp); i++ {
 		row := make([]int, len(t)+1)
+		row[len(t)] = 1
 
-		for j := 0; j < len(row); j++ {
-			row[j] = -1
-		}
-
-		cache[i] = row
+		dp[i] = row
 	}
 
-	return dp(s, t, 0, 0, cache)
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := len(t) - 1; j >= 0; j-- {
+			if s[i] == t[j] {
+				dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+			} else {
+				dp[i][j] = dp[i+1][j]
+			}
+		}
+	}
+
+	return dp[0][0]
 }
